4cd12ddcd2b78515c5f97c01008a2445714c69d7
ernest.aydinov <boomzin@gmail.com>
2022-10-21 19:39:54 +0300

    init commit

---
+++ src/main/java/com/example/mediateltesttask/MediateltesttaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.mediateltesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediateltesttaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediateltesttaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mediateltesttask/MediateltesttaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mediateltesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediateltesttaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
